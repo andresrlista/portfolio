58fe2651b467251223e491f19be784e7ade6f3be
andres <andresrlista@gmail.com>
2024-05-22 13:39:01 -0400

    fixed margins

---
+++ src/data/timeline_data.js
@@ -0,0 +1,38 @@
+export const TIMELINE_DATA_EXPERIENCE = [
+  {
+    title: 'Full-stack developer',
+    company: 'TDBIM Solutions LLC',
+    mode: 'Remote',
+    location: 'Office - United States',
+    date: '2023 - Present',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.',
+  },
+  {
+    title: 'Web developer',
+    company: 'Lessa Investments LLC',
+    mode: 'Remote',
+    location: 'United States',
+    date: '2022 - 2023',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.',
+  },
+  {
+    title: 'Full-stack developer',
+    company: 'CEYBA LLC',
+    mode: 'Remote',
+    location: 'United States',
+    date: '2017 - 2019',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.',
+  },
+  {
+    title: 'Back-end developer | Android developer',
+    company: 'VisualSat Peru SAC',
+    mode: 'Remote',
+    location: 'United States',
+    date: '2015 - 2017',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.',
+  },
+];
